c56eb1f26c4725a7fb5275037495cda982b09ef9
Ritesh Tiwari <ritiktiwari2212@gmail.com>
2023-08-17 00:56:10 +0530

    changes in config

--- config/db.js
+++ config/db.js
@@ -1,18 +1,33 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// function connectDB() {
+//   mongoose.connect(process.env.MONGO_CONNECTION_URL);
+//   const connection = mongoose.connection;
+//   connection
+//     .once("open", () => {
+//       console.log("Database connected");
+//     })
+//     .on("error", (err) => {
+//       console.log(err)
+//       console.log("Connection failed");
+//     });
+// }
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
-function connectDB() {
-  mongoose.connect(process.env.MONGO_CONNECTION_URL,{
-    bufferTimeoutMS: 30000,
-  });
-  const connection = mongoose.connection;
-  connection
-    .once("open", () => {
-      console.log("Database connected");
-    })
-    .on("error", (err) => {
-      console.log(err)
-      console.log("Connection failed");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Connection failed:", error.message);
+  }
 }
 
 module.exports = connectDB;
